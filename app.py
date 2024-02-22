07920c255ed7aebcf4ff27ebb51651a32f2ab06f
david kundrats <davidkundrats@gmail.com>
2024-02-22 12:25:35 -0500

    chatbot instantion changes

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, request, jsonify
-from main import process_message
+from create_chatbot import create_chatbot
 
 app = Flask(__name__)
-
+chatbot = create_chatbot()
 @app.route('/chat', methods=['POST'])
 def chat():
     data = request.get_json()
     user_message = data['message']
-    bot_response = process_message(user_message)
+    bot_response = chatbot.process_message(user_message)
     return jsonify({'response': bot_response})
 if __name__ == '__main__':
     app.run(debug = True)
