57bb512fd99397581145e28e65901264c7c21491
david kundrats <davidkundrats@gmail.com>
2024-02-22 12:22:33 -0500

    abstracted chatbot class

---
+++ chatbot/Chatbot.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+
+class Chatbot():
+    """ The abstract class providing the template for the chatbot, all instances of a chatbot must implement this class """
+    def __init__(self, embeddings, model, vector_store):
+        self.embeddings = embeddings
+        self.model = model
+        self.vector_store = vector_store
+    @abstractmethod
+    def generate_prompt(self, query):
+        #Abstract class to generate prompt. Should be implemented by subclass
+        pass
+    @abstractmethod
+    def process_message(self, message):
+        #Abstract class to generate prompt. Should be implemented by subclass
+        pass
---
+++ chatbot/__init__.py
@@ -0,0 +1,2 @@
+from .Chatbot import Chatbot
+from .oai_pc.oai_pc import OAI_PC_Chatbot
