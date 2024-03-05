32f2f288d139cd54e284ebacf9dbe54452a81d47
davidkundrats <davidkundrats@gmail.com>
2024-03-05 17:37:52 -0500

    added docstring to openai-pc class and fix grammar in prompt

--- chatbot/oai_pc/oai_pc.py
+++ chatbot/oai_pc/oai_pc.py
@@ -5,12 +5,13 @@ from pinecone import Pinecone as Pc
 
 
 class OAI_PC_Chatbot(Chatbot):
+    """ Chatbot that uses OpenAI's model and Pinecone's similarity search to answer questions. """
     def __init__(self, embeddings, model, vector_store):
         super().__init__(embeddings, model, vector_store)
     def generate_prompt(self,query): 
         results = self.vector_store.similarity_search(query, k=5)
         knowledge = "/n".join(doc.page_content for doc in results)
-        prompt = f""" Use only this context given to you to answer the question. If you cannot answer the query using the knowledge ba        se, clearly state so and end your answer. 
+        prompt = f""" Use only this context given to you to answer the question. If you cannot answer the query using the knowledge base, clearly state so and end your answer. 
 
         Knowledge: {knowledge}
 
